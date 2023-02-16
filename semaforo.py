2f107a973b26f204f546fbc00bb389830e3c8292
Javier <faboll01@ucm.es>
2023-02-16 13:45:25 +0100

    Practica decker semafor y lock

---
+++ semaforo.py
@@ -0,0 +1,38 @@
+from multiprocessing import Process
+from multiprocessing import Value, BoundedSemaphore
+import time, random
+
+N = 8
+
+
+def task(common, tid, sem):
+    a = 0
+    for i in range(100):
+        print(f"{tid}−{i}: Non−critical Section")
+        time.sleep(random.random())
+        print(f"{tid}−{i}: End of non−critical Section")
+        sem.acquire()
+        print(f"{tid}−{i}: critical section")
+        v = common.value + 1        
+        time.sleep(random.random())
+        print(f"{tid}−{i}: Inside critical section")
+        common.value = v
+        print(f"{tid}−{i}: End of critical section")
+        sem.release()
+
+def main():
+    lp = []
+    common = Value("i", 0)
+    sem = BoundedSemaphore(1)
+    for tid in range(N):
+        lp.append(Process(target=task, args=(common, tid, sem)))
+    print (f"Valor inicial del contador {common.value}")
+    for p in lp:
+        p.start()
+    for p in lp:
+        p.join()
+    print (f"Valor final del contador {common.value}")
+    print ("fin")
+
+if __name__ == "__main__":
+    main()
